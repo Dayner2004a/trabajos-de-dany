50fb76184fe9e9f212840b60e46b3fdc5af47be4
dayner <daynerebenezer@gmail.com>
2024-08-26 20:06:46 -0600

    carpeta completa

--- git.js
+++ git.js
@@ -3,8 +3,9 @@ let A =[[3,4,5,6,7],[8,9,10]];
 
 for (let fil of A);{
     for (let [i,v] of fil.entries()){
-        console.log(${i}=${v}
+        console.log('${i}=${v}')
             
-        )
+        
     }
 }
+
